1f4f7b744d919cdb8712d41ad82c0df3493ddb8f
Artrm Poyarkov <poart1405@gmail.com>
2020-05-25 13:58:58 +0300

    add empty banner

--- components/morphs/morphs.js
+++ components/morphs/morphs.js
@@ -41,6 +41,36 @@ class Morphs extends Component {
             return <Spinner/>;
         }
 
+        if(genes.length === 0 && subcategories.length === 0 && !request && activeKind.title_eng) {
+            return (
+                <React.Fragment>
+                    <Head>
+                        <title>Похоже в категории {activeKind.title_rus} нет активных морф</title>
+                    </Head>
+                    <Row>
+                        <Col xs={12} className="text-center d-flex h-100">
+                            <h2 className="m-auto">Похоже в категории {activeKind.title_rus} нет активных морф</h2>
+                        </Col>
+                    </Row>
+                </React.Fragment>
+            )
+        }
+
+        if(genes.length === 0 && subcategories.length === 0 && !request && !activeKind.title_eng) {
+            return (
+                <React.Fragment>
+                    <Head>
+                        <title>Похоже в категории {activeKind.title_rus} нет животных</title>
+                    </Head>
+                    <Row>
+                        <Col xs={12} className="text-center d-flex h-100">
+                            <h2 className="m-auto">Похоже в категории {activeKind.title_rus} нет животных</h2>
+                        </Col>
+                    </Row>
+                </React.Fragment>
+            )
+        }
+
         return (
             <React.Fragment>
                 <Head>
@@ -60,47 +90,54 @@ class Morphs extends Component {
                         )
                         : null
                 }
-                <h2 className="mb-2">Подкатегории и локалитеты:</h2>
-                <Row className="flex-column subcategories align-items-center">
-                    {
-                        subcategories.map( (item) => (
-                            <Col key={item.title} xs={12} md={4} className="subcategories-item">
-                                <div className="subcategories-title">
-                                    <Link href="/[group]/[kind]/subcategories/[subcategoryTitle]" as={`/${group}/${kind}/subcategories/${this.pipes.toUrl(item.title)}`}>
-                                        <a className="d-flex justify-content-between w-100">
-                                            <h3>{item.title}</h3>
-                                            <div className="morph-indicator-count morph-other-normal ml-2">{item.count}</div>
-                                        </a>
-                                    </Link>
-                                </div>
-                                {
-                                    item.localities ?
-                                        (
-                                            <ul className="pl-4 localities">
+                {
+                    subcategories.length > 0 ?
+                        (
+                            <React.Fragment>
+                                <h2 className="mb-2">Подкатегории и локалитеты:</h2>
+                                <Row className="flex-column subcategories align-items-center">
+                                    {
+                                        subcategories.map( (item) => (
+                                            <Col key={item.title} xs={12} md={4} className="subcategories-item">
+                                                <div className="subcategories-title">
+                                                    <Link href="/[group]/[kind]/subcategories/[subcategoryTitle]" as={`/${group}/${kind}/subcategories/${this.pipes.toUrl(item.title)}`}>
+                                                        <a className="d-flex justify-content-between w-100">
+                                                            <h3>{item.title}</h3>
+                                                            <div className="morph-indicator-count morph-other-normal ml-2">{item.count}</div>
+                                                        </a>
+                                                    </Link>
+                                                </div>
                                                 {
-                                                    item.localities.map( (locality) => {
-                                                        if (locality.count) {
-                                                            return (
-                                                                <li className="localities-item">
-                                                                    <Link key={locality.title} href="/[group]/[kind]" as={`/${group}/${kind}?locality=${locality.id}`} >
-                                                                        <a className="d-flex justify-content-between">
-                                                                            <h3>{locality.title}</h3>
-                                                                            <div className="morph-indicator-count morph-other-normal ml-2">{locality.count}</div>
-                                                                        </a>
-                                                                    </Link>
-                                                                </li>
-                                                            )
-                                                        }
-                                                    })
+                                                    item.localities ?
+                                                        (
+                                                            <ul className="pl-4 localities">
+                                                                {
+                                                                    item.localities.map( (locality) => {
+                                                                        if (locality.count) {
+                                                                            return (
+                                                                                <li className="localities-item">
+                                                                                    <Link key={locality.title} href="/[group]/[kind]" as={`/${group}/${kind}?locality=${locality.id}`} >
+                                                                                        <a className="d-flex justify-content-between">
+                                                                                            <h3>{locality.title}</h3>
+                                                                                            <div className="morph-indicator-count morph-other-normal ml-2">{locality.count}</div>
+                                                                                        </a>
+                                                                                    </Link>
+                                                                                </li>
+                                                                            )
+                                                                        }
+                                                                    })
+                                                                }
+                                                            </ul>
+                                                        )
+                                                        : null
                                                 }
-                                            </ul>
-                                        )
-                                        : null
-                                }
-                            </Col>
-                        ))
-                    }
-                </Row>
+                                            </Col>
+                                        ))
+                                    }
+                                </Row>
+                            </React.Fragment>
+                        ) : null
+                }
             </React.Fragment>
         );
     }
