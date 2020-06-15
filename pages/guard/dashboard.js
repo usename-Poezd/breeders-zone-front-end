c7c020fc541d88415e86506ff553d0f24b577310
Artrm Poyarkov <poart1405@gmail.com>
2020-06-15 22:01:17 +0300

    add report modal component

--- pages/guard/dashboard.js
+++ pages/guard/dashboard.js
@@ -1,16 +1,14 @@
-import Header from "../../components/header";
-import Head from "next/head";
 import {Container} from "react-bootstrap";
 import React from "react";
 import GuardDashboard from "../../components/guard/guard-dashboard";
+import ReportModal from "../../components/report-modal";
 
 const Dashboard = (props) => {
     return (
-        <React.Fragment>
-            <Container>
-                <GuardDashboard/>
-            </Container>
-        </React.Fragment>
+        <Container>
+            <ReportModal/>
+            <GuardDashboard/>
+        </Container>
     );
 };
 
