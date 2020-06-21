074006c684f06d343d7385924bfa1681cc3504d1
Artrm Poyarkov <poart1405@gmail.com>
2020-06-21 13:51:17 +0300

    create prodile divorces page

---
+++ pages/profile/divorces/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+import Header from "../../../components/header/header";
+import Head from "next/head";
+import ShopDivorces from "../../../components/shop-divorces/shop-divorces";
+
+const DivorcesPage = () => {
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Мои разводы</title>
+            </Head>
+            <Container>
+                <ShopDivorces/>
+            </Container>
+        </React.Fragment>
+    )
+};
+
+
+export default DivorcesPage;
