29417d17e3b90d69f3699ef06ce1b9ce9427c787
Artrm Poyarkov <poart1405@gmail.com>
2020-06-21 13:50:52 +0300

    update /divorces page

--- pages/divorces/index.js
+++ pages/divorces/index.js
@@ -1,21 +1,75 @@
-import React from "react";
-import {Container} from "react-bootstrap";
-import Header from "../../components/header/header";
-import Head from "next/head";
-import ShopDivorces from "../../components/shop-divorces/shop-divorces";
+import React, {Component} from "react";
+import {DataService} from "../../services";
+import {Col, Container, Form, Row, Spinner as BootstrapSpinner} from "react-bootstrap";
+import Link from "next/link";
+import LazyImg from "../../components/lazy-img";
+import {formatDate} from "react-day-picker/moment";
+import Pagination from "../../components/pagination";
+import {withRouter} from "next/router";
+import Spinner from "../../components/spinner";
+import DivorcesListItem from "../../components/divorces-list-item";
+import ReportModal from "../../components/report-modal/report-modal";
 
-const DivorcesPage = () => {
-    return (
-        <React.Fragment>
-            <Head>
-                <title>Мои разводы</title>
-            </Head>
+class Divorces extends Component {
+    state = {
+        request: false
+    };
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.router.query !== this.props.router.query) {
+            this.setState({request: false})
+        }
+    }
+
+    changeRequest = () => this.setState({request: true});
+
+    render() {
+        const {divorces} = this.props;
+        const {request} = this.state;
+
+        return (
             <Container>
-                <ShopDivorces/>
+                <ReportModal/>
+                <Row>
+                    <Col xs={12} className="my-3">
+                        <h1>Разводы:</h1>
+                    </Col>
+                </Row>
+                <Row className="position-relative">
+                    {
+                        request && divorces.data.length > 0 ?
+                            <div className="load">
+                                <BootstrapSpinner animation="border" className="m-auto"/>
+                            </div>
+                            : null
+                    }
+                    {
+                        request && divorces.data.length === 0 ?
+                            <Spinner/>
+                            : null
+                    }
+                    {
+                        divorces.data.map( (item) => <DivorcesListItem key={item.id} {...item}/>)
+                    }
+                </Row>
+                {
+                    divorces.last_page !== 1 ?
+                        <Pagination className="d-flex justify-content-center mb-2" totalItems={divorces.last_page} pageSize={1} defaultActivePage={divorces.current_page} changeRequest={this.changeRequest}/>
+                        : null
+                }
             </Container>
-        </React.Fragment>
-    )
+        );
+    }
 };
 
+export const getServerSideProps = async (ctx) => {
+    const dataService = new DataService();
+    const divorces = await dataService.getDivorces(ctx.query);
+    return {
+        props: {
+            divorces
+        }
+    }
+};
 
-export default DivorcesPage;
+export default withRouter(Divorces);
