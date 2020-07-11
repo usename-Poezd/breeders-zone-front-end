439e3e8428f5575631da1b1dabe2fec6e3086089
Artrm Poyarkov <poart1405@gmail.com>
2020-07-11 15:40:38 +0300

    update lazy img

--- components/lazy-img/lazy-img.js
+++ components/lazy-img/lazy-img.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-const LazyImg = ({ src, className, ...other }) => {
-    return <img className={"lazyload" + (className ? ` ${className}` : null)} data-src={src} {...other}/>;
+const LazyImg = ({ src, ...other }) => {
+    other.className += ' lazyload';
+
+    return <img data-src={src} {...other}/>;
 };
 
 export default LazyImg;
