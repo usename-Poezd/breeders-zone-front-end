3cdc99a7af2990e96620ed2a344833a55b3a2ab2
Artrm Poyarkov <poart1405@gmail.com>
2020-07-11 15:43:19 +0300

    add countries

--- pages/profile/shop.js
+++ pages/profile/shop.js
@@ -2,6 +2,9 @@ import React from "react";
 import {Container} from "react-bootstrap";
 import Header from "../../components/header/header";
 import ShopProfile from "../../components/shop-profile";
+import wrapper from "../../store";
+import {DataService} from "../../services";
+import {setCountries} from "../../actions";
 
 const ShopProfilePage = (props) => {
     return (
@@ -10,5 +13,11 @@ const ShopProfilePage = (props) => {
         </Container>
     )
 };
+
+export const getStaticProps = wrapper.getStaticProps(async ({store}) => {
+    const dataService = await new DataService();
+    const data = await dataService.getCountries();
+    store.dispatch(setCountries(data))
+});
 
 export default ShopProfilePage;
