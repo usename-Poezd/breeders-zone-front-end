99d1a5db00fd19623e35f831b7c82282a6a0ff11
Artrm Poyarkov <poart1405@gmail.com>
2020-07-11 15:44:53 +0300

    upgrade message

--- utils/upgraded-message.js
+++ utils/upgraded-message.js
@@ -3,6 +3,7 @@ import {Message} from "react-chat-ui";
 export default class UpgradedMassage extends Message{
     constructor(data) {
         super(data);
+        this.product = data.product;
         this.status = data.status;
         this.messageId = data.messageId;
         this.checked = data.checked;
