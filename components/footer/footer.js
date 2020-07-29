fe87ed1d09dc620f2532a9b31bc0f5a9c7a5af77
Artrm Poyarkov <poart1405@gmail.com>
2020-07-29 15:59:46 +0300

    create footer

---
+++ components/footer/footer.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Link from "next/link";
+import {connect} from "react-redux";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebook, faVk} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = (props) => {
+    const {auth} = props;
+
+    return (
+        <div className="footer">
+            <div className="footer-body">
+                <div className="d-flex flex-wrap justify-content-center">
+                    <Link href="/faq">
+                        <a className="footer-item">FAQ</a>
+                    </Link>
+                    <Link href="/documents">
+                        <a className="footer-item">Юредические документы</a>
+                    </Link>
+                    {
+                        !auth.isLogin ?
+                            <React.Fragment>
+                                <Link href="/login">
+                                    <a className="footer-item">Войти</a>
+                                </Link>
+                                <Link href="/register">
+                                    <a className="footer-item">Зарегистрироваться</a>
+                                </Link>
+                            </React.Fragment>
+                            : null
+                    }
+                </div>
+                <div className="d-flex flex-wrap justify-content-center">
+                    <a href="https://fb.com" className="footer-item">
+                        <FontAwesomeIcon icon={faFacebook} className="social-facebook" size="lg"/>
+                    </a>
+                    <a href="https://vk.com" className="footer-item">
+                        <FontAwesomeIcon icon={faVk} className="social-vk" size="lg"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+const mapStateToProps = ({auth}) => ({
+    auth
+});
+
+export default connect(mapStateToProps)(Footer);
