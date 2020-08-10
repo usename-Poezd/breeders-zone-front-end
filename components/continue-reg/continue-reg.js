5b0297558e63ffcd0b7308b2b21d5efe9333be29
Artrm Poyarkov <poart1405@gmail.com>
2020-08-10 23:00:09 +0300

    remove hoc

--- components/continue-reg/continue-reg.js
+++ components/continue-reg/continue-reg.js
@@ -3,82 +3,60 @@ import { useForm } from 'react-hook-form';
 import { Form } from 'react-bootstrap';
 import { withHookForm } from '../hoc-helpers';
 import GroupFormControl from "../group-form-control";
-import Router from 'next/router'
+import Router, {withRouter} from 'next/router'
 
-class ContinueRegistration extends Component {
+const ContinueRegistration = (props) => {
+    const registerContinue = data => {
+        const {router} = props;
 
-    state = {
-        regName: '',
-        regSurname: '',
-        regEmail: '',
-        isReg: false
+        router.push(`/registration?name=${data.name}&surname=${data.surname}&email=${data.email}`)
     };
 
+    const { register, handleSubmit, errors } = useForm();
 
-    checkRedirect = () => {
-        const {regName, regSurname, regEmail} = this.state;
-        if(this.state.isReg){
-            Router.push(`/registration?name=${regName}&surname=${regSurname}&email=${regEmail}`)
-        }
-    };
+    return (
+        <div className="reg form-container">
+            <h2 className="form-container-title">Впервые на Breeders Zone?</h2>
+            <Form onSubmit={handleSubmit(registerContinue)} className="form login-form">
+                <GroupFormControl
+                    errors={errors}
+                    controls={{
+                        type: "text",
+                        name: "name",
+                        placeholder: "Имя",
+                        ref: register({
+                            required: true
+                        })
+                    }}
+                />
+                <GroupFormControl
+                    errors={errors}
+                    controls={{
+                        type: "text",
+                        name: "surname",
+                        placeholder: "Фамилия",
+                        ref: register({
+                            required: true
+                        })
+                    }}
+                />
+                <GroupFormControl
+                    label="Электронная почта"
+                    errors={errors}
+                    controls={{
+                        type: "text",
+                        name: "email",
+                        placeholder: "myMail123@mail.com",
+                        ref: register({
+                            required: true,
+                            pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                        })
+                    }}
+                />
+                <input type="submit" value="Продолжить регестрацию" className="btn btn-success btn-bn w-100"/>
+            </Form>
+        </div>
+    )
+};
 
-    registerContinue = data => {
-        this.setState({
-            regName: data.name,
-            regSurname: data.surname,
-            regEmail: data.email,
-            isReg: true
-        });
-    };
-
-    render(){
-        const { register, handleSubmit, watch, errors } = this.props;
-
-        return (
-            <div className="reg form-container">
-                <h2 className="form-container-title">Впервые на Breeders Zone?</h2>
-                <Form onSubmit={handleSubmit(this.registerContinue)} className="form login-form">
-                    <GroupFormControl
-                        errors={errors}
-                        controls={{
-                            type: "text",
-                            name: "name",
-                            placeholder: "Имя",
-                            ref: register({
-                                required: true
-                            })
-                        }}
-                    />
-                    <GroupFormControl
-                        errors={errors}
-                        controls={{
-                            type: "text",
-                            name: "surname",
-                            placeholder: "Фамилия",
-                            ref: register({
-                                required: true
-                            })
-                        }}
-                    />
-                    <GroupFormControl
-                        label="Электронная почта"
-                        errors={errors}
-                        controls={{
-                            type: "text",
-                            name: "email",
-                            placeholder: "myMail123@mail.com",
-                            ref: register({
-                                required: true,
-                                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                            })
-                        }}
-                    />
-                    <input type="submit" value="Продолжить регестрацию" className="btn btn-success btn-bn w-100"/>
-                    {this.checkRedirect()}
-                </Form>
-            </div>
-        )
-    }
-}
-
-export default withHookForm(ContinueRegistration, useForm);
+export default withRouter(ContinueRegistration);
--- components/login/login.js
+++ components/login/login.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 import { Form } from 'react-bootstrap';
-import {withHookForm, withGetData} from '../hoc-helpers';
+import {withGetData} from '../hoc-helpers';
 import { useForm } from "react-hook-form";
 import {connect} from "react-redux";
 import {getUser} from "../../actions";
 import GroupFormControl from "../group-form-control";
 import {withRouter} from 'next/router'
 
-class Login extends Component {
+const Login = (props) => {
+    const [serverError, setServerError] = useState(false);
+    const {register, handleSubmit, errors} = useForm();
 
-    state = {
-        serverError: null
-    };
-
-    login = data => {
-        const { postLogin, getUser, router } = this.props;
+    const login = data => {
+        const { postLogin, getUser, router } = props;
         postLogin(data)
             .then( () => {
                 router.push('/');
@@ -25,67 +23,58 @@ class Login extends Component {
                 if (error.response.status === 403) {
                     router.push('/verify');
                 }
-                this.setState({ serverError: error });
+                setServerError(error);
             });
     };
 
-    render() {
-        const { serverError } = this.state;
-        const { register, handleSubmit, errors, isLogin, router } = this.props;
-
-        if (isLogin && typeof window !== 'undefined') {
-            router.push('/');
-        }
-
-        return (
-            <div className="login form-container">
-                <h2 className="form-container-title">Вход</h2>
-                {
-                    serverError ? (
-                        <div className="feather-shadow text-center p-3 mb-3 border-10">
-                            <h3 className="text-danger mb-1">Не удается войти.</h3>
-                            <p className="form-err ">Пожалуйста, проверьте правильность написания логина и пароля.</p>
-                        </div>
-                    ) : null
-                }
-                <Form onSubmit={handleSubmit(this.login)} className="form login-form">
-                    <GroupFormControl
-                        errors={errors}
-                        controls={{
-                            type: "text",
-                            name: "email",
-                            placeholder: "Электронная почта",
-                            ref: register({
-                                required: true,
-                                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                            })
-                        }}
-                    />
-
-                    <GroupFormControl
-                        errors={errors}
-                        controls={{
-                            type: "password",
-                            name: "password",
-                            placeholder: "Пароль",
-                            ref: register({
-                                required: true
-                            })
-                        }}
-                    />
-
-                    <div className="controls d-flex align-items-center">
-                        <input type="submit" value="Войти" className="btn btn-main w-100"/>
-                        <Link href="/reset">
-                            <a  className="forgot-pass w-100">Забыли пароль?</a>
-                        </Link>
+    return (
+        <div className="login form-container">
+            <h2 className="form-container-title">Вход</h2>
+            {
+                serverError ? (
+                    <div className="feather-shadow text-center p-3 mb-3 border-10">
+                        <h3 className="text-danger mb-1">Не удается войти.</h3>
+                        <p className="form-err ">Пожалуйста, проверьте правильность написания логина и пароля.</p>
                     </div>
+                ) : null
+            }
+            <Form onSubmit={handleSubmit(login)} className="form login-form">
+                <GroupFormControl
+                    errors={errors}
+                    controls={{
+                        type: "text",
+                        name: "email",
+                        placeholder: "Электронная почта",
+                        ref: register({
+                            required: true,
+                            pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                        })
+                    }}
+                />
 
-                </Form>
-            </div>
-        );
-    }
-}
+                <GroupFormControl
+                    errors={errors}
+                    controls={{
+                        type: "password",
+                        name: "password",
+                        placeholder: "Пароль",
+                        ref: register({
+                            required: true
+                        })
+                    }}
+                />
+
+                <div className="controls d-flex align-items-center">
+                    <input type="submit" value="Войти" className="btn btn-main w-100"/>
+                    <Link href="/reset">
+                        <a  className="forgot-pass w-100">Забыли пароль?</a>
+                    </Link>
+                </div>
+
+            </Form>
+        </div>
+    );
+};
 
 const mapMethodsToProps = (getData) => {
     return {
@@ -101,7 +90,7 @@ const mapStateToProps = ({auth: {isLogin}}) => ({
 export default connect(mapStateToProps, {getUser})(
    withRouter(
        withGetData(
-           withHookForm(Login, useForm),
+           Login,
            mapMethodsToProps
        )
    )
