a2a389ae577bb189debb697194e29fde3ade2ef7
Artrm Poyarkov <poart1405@gmail.com>
2020-09-02 19:48:34 +0300

    fix types

--- reducers/stats/types.ts
+++ reducers/stats/types.ts
@@ -6,13 +6,9 @@ export interface IStatsState {
     isMobile: boolean
 }
 
-interface IHydrate {
-    stats: {}
-}
-
 export type IStatsAction = {
     type: 'SET_IS_MOBILE'|typeof HYDRATE,
-    payload: IHydrate|boolean
+    payload: any
 }
 
 
