cea798e1d4ecb68b39aa4772feb012ca78ce2864
Artrm Poyarkov <poart1405@gmail.com>
2020-11-12 18:08:35 +0300

    delete hoc in components

--- components/hoc-helpers/index.js
+++
@@ -1,9 +0,0 @@
-import withGetData from './with-get-data';
-import withHookForm from './with-hook-form'
-import withErrorBoundry from './with-error-boundry';
-
-export {
-    withGetData,
-    withHookForm,
-    withErrorBoundry
-};
