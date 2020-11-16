4ddf0e94b1341bfe5122a14b3400e571001ca7ab
Artrm Poyarkov <poart1405@gmail.com>
2020-11-16 20:47:16 +0300

    update search

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {FC} from "react";
 import {Container} from "react-bootstrap";
 import TraitItems from "../components/traits-list";
 import {DataService} from "../services";
 import Head from "next/head";
 import {ProductList} from "../components/ProductList";
+import {GetProductsDataType} from "../types";
 const qs = require('qs');
 
-const SearchPage = (props) => {
+type SearchPagePropsType = {
+    products: GetProductsDataType
+}
+
+const SearchPage: FC<SearchPagePropsType> = ({products}) => {
     return (
         <Container>
             <Head>
                 <title>Поиск | Breeders Zone</title>
                 <meta name="description" content="Breeders Zone это маркетплейс где вы можете бысто найти и продать животное, больше никаних групп и форумов, все в одном месте"/>
             </Head>
-            <ProductList {...props}/>
+            <ProductList products={products.data} meta={products.meta}/>
         </Container>
     )
 };
