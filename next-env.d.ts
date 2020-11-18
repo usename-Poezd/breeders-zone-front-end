f31e08048dd0a00aa0d10a57a794eab8bbae7b60
Artrm Poyarkov <poart1405@gmail.com>
2020-11-18 08:02:28 +0300

    updaate ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+declare module 'next-ym';
+declare module 'currency-symbol-map';
+declare module "moment" { export = moment; }
